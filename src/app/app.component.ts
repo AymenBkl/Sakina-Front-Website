7a65f90b5a1539351bf8acd5ceb83f9dd24ef438
Aymene Abdelmoumene Boukhelkhal <aymenxyz6@gmail.com>
2021-11-08 20:14:08 +0100

    worked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,21 +14,28 @@ export class AppComponent {
     domain:'',
   }
   emails:[];
+  searchedEmails = [];
   constructor(private httpClient: HttpClient) {
 
   }
 
   search() {
     console.log(this.searchData);
+    this.emails = [];
+    this.searchedEmails = [];
     this.httpClient.post('http://localhost:3000/scrap',this.searchData)
       .subscribe((data:any) => {
         console.log(data);
         if (data && data.success && data.data){
           console.log('here',data.data.selectors);
           this.emails = data.data.selectors;
+          this.searchEmails();
         }
       },error => {
         console.log(error);
       })
+  }
+  searchEmails(){
+    this.searchedEmails = this.emails.filter((email:any) => email.selectorvalue.includes(this.searchData.firstName) && email.selectorvalue.includes(this.searchData.lastName) );
   }
 }
