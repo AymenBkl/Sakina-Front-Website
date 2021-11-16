128b25c691320c12ef824a671b91ad4ae1ec7b51
Aymene Abdelmoumene Boukhelkhal <aymenxyz6@gmail.com>
2021-11-16 20:12:23 +0100

    worked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,11 @@ export class AppComponent {
   error = { msg: '' };
   success = false;
   emailToSend = '';
+  filterdRows = [];
+  columns = [];
+  errEmail = '';
+  serverRes = {err:false,msg: ''};
+  baseUrl = 'http://localhost:3000/email/';
   constructor(private httpClient: HttpClient) {
 
   }
@@ -22,11 +27,11 @@ export class AppComponent {
       if (excelfile && excelfile.length > 0) {
         readXlsxFile(excelfile[0])
           .then((rows) => {
-            const columns = rows[0];
+            this.columns = rows[0];
             rows.shift();
             if (rows && rows.length > 0) {
-              const filtersRows = rows.filter(row => !row[0].toString().includes('#'));
-              if (filtersRows && filtersRows.length > 0) {
+              this.filterdRows = rows.filter(row => !row[0].toString().includes('#'));
+              if (this.filterdRows && this.filterdRows.length > 0) {
                 this.error.msg = '';
                 this.success = true;
               }
@@ -55,5 +60,28 @@ export class AppComponent {
       this.success = false;
       this.error.msg = 'No File Found';
     }
+  }
+
+  sendEmail() {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const newEmail = this.emailToSend.toLowerCase();
+    if (re.test(newEmail)) {
+      this.errEmail = '';
+      if (this.filterdRows.length > 0){
+        this.sendDataServer(newEmail);
+      }
+    }
+    else {
+      this.errEmail = 'Email format is wrong';
+    }
+  }
+
+  sendDataServer(email: string) {
+    this.httpClient.post(this.baseUrl, {rows:this.filterdRows,columns:this.columns,email:email})
+      .subscribe(res => {
+
+      },err => {
+
+      })
   }
 }
