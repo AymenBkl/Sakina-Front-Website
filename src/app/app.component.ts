341089a80b48abb3ae8c646b0cf741811cb68b85
Aymene Abdelmoumene Boukhelkhal <aymenxyz6@gmail.com>
2021-11-16 19:50:22 +0100

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,58 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-
+import readXlsxFile from 'read-excel-file'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'scrap-ui';
-  searchData = {
-    firstName:'',
-    lastName:'',
-    domain:'',
-  }
-  emails:[];
-  searchedEmails = [];
+  error = { msg: '' };
+  success = false;
   constructor(private httpClient: HttpClient) {
 
   }
 
-  search() {
-    console.log(this.searchData);
-    this.emails = [];
-    this.searchedEmails = [];
-    this.httpClient.post('http://localhost:3000/scrap',this.searchData)
-      .subscribe((data:any) => {
-        console.log(data);
-        if (data && data.success && data.data){
-          console.log('here',data.data.selectors);
-          this.emails = data.data.selectors;
-          this.searchEmails();
-        }
-      },error => {
-        console.log(error);
-      })
-  }
-  searchEmails(){
-    this.searchedEmails = this.emails.filter((email:any) => email.selectorvalue.includes(this.searchData.firstName) && email.selectorvalue.includes(this.searchData.lastName) );
+  readFileExcel(event) {
+    const element = event.currentTarget as HTMLInputElement;
+    let fileList: FileList | null = element.files;
+    if (fileList) {
+      const excelfile = Object.values(fileList);
+      if (excelfile && excelfile.length > 0) {
+        readXlsxFile(excelfile[0])
+          .then((rows) => {
+            const columns = rows[0];
+            rows.shift();
+            if (rows && rows.length > 0) {
+              const filtersRows = rows.filter(row => !row[0].toString().includes('#'));
+              if (filtersRows && filtersRows.length > 0) {
+                this.error.msg = '';
+                this.success = true;
+              }
+              else {
+                this.success = false;
+                this.error.msg = 'All rows includes a #xxx number';
+              }
+            }
+            else {
+              this.success = false;
+              this.error.msg = "Please chooce a file with rows";
+            }
+            console.log(rows);
+          })
+          .catch(err => {
+            this.success = false;
+            this.error.msg = 'Something Went Wrong ! Please chooce an xlsx file';
+          })
+      }
+      else {
+        this.success = false;
+        this.error.msg = 'No File Found';
+      }
+    }
+    else {
+      this.success = false;
+      this.error.msg = 'No File Found';
+    }
   }
 }
